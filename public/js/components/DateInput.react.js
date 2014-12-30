821d96f33a379f7064046ab34d477cc8efe307a1
Robert Niimi <Robert Niimi>
2014-12-30 14:21:28 -0800

    aligned date input and merchant-category buttons

--- public/js/components/DateInput.react.js
+++ public/js/components/DateInput.react.js
@@ -4,11 +4,18 @@ var BarGraph = React.createClass({
 
   render: function() {
     return (
-      <div id="date-input">
-        <span>Date Range</span>
-        <span>Filter</span>
-        <span className="category-merchant-button">By Category</span>
-        <span className="category-merchant-button">By Merchant</span>
+      <div id="date-filter-input">
+        <span id="date-range-wrapper">
+          <span><b>Date Range</b></span>
+          <div className="date-input">May 1, 2012</div>
+          <div className="date-input">Sep 1, 2014</div>
+        </span>
+        <div align="right" id="filter-wrapper">
+          <span><b>Filter</b></span>
+          <div className="category-merchant-button">By Category</div>
+          <div className="category-merchant-button">By Merchant</div>
+        </div>
+        <div className="divider"></div>
       </div>
     );
   }
