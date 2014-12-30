cb1f28f39f22b979c70a7e541efb80f84e9102d3
Robert Niimi <Robert Niimi>
2014-12-30 15:43:33 -0800

    set fixed width and height to category & merchant buttons and added margin to labels

--- public/js/components/DateInput.react.js
+++ public/js/components/DateInput.react.js
@@ -6,12 +6,12 @@ var BarGraph = React.createClass({
     return (
       <div id="date-filter-input">
         <span id="date-range-wrapper">
-          <span><b>Date Range</b></span>
+          <span className="date-filter-label"><b>Date Range</b></span>
           <div className="date-input">May 1, 2012</div>
           <div className="date-input">Sep 1, 2014</div>
         </span>
-        <div align="right" id="filter-wrapper">
-          <span><b>Filter</b></span>
+        <div id="filter-wrapper">
+          <span className="date-filter-label"><b>Filter</b></span>
           <div className="category-merchant-button">By Category</div>
           <div className="category-merchant-button">By Merchant</div>
         </div>
