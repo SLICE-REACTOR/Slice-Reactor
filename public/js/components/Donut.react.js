0763a83c633b073ce396cbab2455102a23545787
Krystal Raphael <krystal.raphael@gmail.com>
2014-12-30 18:54:50 -0800

    adds sample graphs

--- public/js/components/Donut.react.js
+++ public/js/components/Donut.react.js
@@ -1,11 +1,50 @@
 var React = require('react');
 var GraphData = require('../stores/GraphDataStore');
-var Donut = React.createClass({
 
+var categoryPrice = function(array){
+  var categoryNames= {};
+  var JSONobj = [];
+  array.forEach(function(item){
+    var key = item.merchant.href;
+    var value = item.orderTotal/100;
+    categoryNames[key] = value;
+  })
+  JSONobj.push(categoryNames);
+  return JSONobj;
+};
+
+var Donut = React.createClass({
+  getInitialState: function(){
+    var graphData = GraphData.getData();
+    var data = categoryPrice(graphData);
+    return {data: data}
+  },
+  componentDidMount: function(){
+    this._renderChart(this.state.data);
+  },
+  _renderChart: function(dataset){
+    var key = Object.keys(dataset[0]);
+    var donutChart = c3.generate({
+      bindto: '#chart_1',
+      data: {
+        json: dataset,
+        keys: {
+          value: key
+        },
+        type: 'donut'
+      },
+      legend: {
+        position: 'right'
+      },
+        donut: {
+          title: "Spending Trends"
+      }
+    })
+  },
   render: function() {
     return (
-      <div id="donut">
-        <h1>Donut</h1>
+      <div>
+        <div id="chart_1"></div>
       </div>
     );
   }
--- public/js/components/LineGraph.react.js
+++ public/js/components/LineGraph.react.js
@@ -1,10 +1,52 @@
 var React = require('react');
-var LineGraph = React.createClass({
+var GraphData = require('../stores/GraphDataStore');
 
+var categoryPrice = function(array){
+  var categoryNames= {};
+  var JSONobj = [];
+  array.forEach(function(item){
+    var key = item.merchant.href;
+    var value = item.orderTotal/100;
+    categoryNames[key] = value;
+  })
+  JSONobj.push(categoryNames);
+  return JSONobj;
+};
+
+var LineGraph = React.createClass({
+  getInitialState: function(){
+    var data = GraphData.getData();
+    return {data: data}
+  },
+  componentDidMount: function(){
+    this._renderChart(this.state.data);
+  },
+  _renderChart: function(dataset){
+    var data = categoryPrice(dataset);
+    var key = Object.keys(data[0]);
+    console.log(dataset);
+
+    var lineChart = c3.generate({
+      bindto: '#chart_2',
+      data: {
+        columns: [
+            ['data1', 30, 200, 100, 400, 150, 250],
+            ['data2', 50, 20, 10, 40, 15, 25]
+        ],
+        regions: {
+            'data1': [{'start':1, 'end':2, 'style':'dashed'},{'start':3}],
+            'data2': [{'end':3}]
+        }
+      },
+      legend: {
+          show: false
+      }
+    });
+  },
   render: function() {
     return (
-      <div id="line-graph">
-        <h1>Line Graph</h1>
+      <div>
+        <div id="chart_2"></div>
       </div>
     );
   }
