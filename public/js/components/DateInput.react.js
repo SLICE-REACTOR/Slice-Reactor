0f30ca7c07a762956f68fcaf8e319c98ac0f8b9a
Robert Niimi <Robert Niimi>
2014-12-30 20:30:50 -0800

    added reset

--- public/js/components/DateInput.react.js
+++ public/js/components/DateInput.react.js
@@ -9,6 +9,7 @@ var BarGraph = React.createClass({
           <span className="date-filter-label"><b>Date Range</b></span>
           <div className="date-input">May 1, 2012</div>
           <div className="date-input">Sep 1, 2014</div>
+          <span id="reset"><b>RESET</b></span>
         </span>
         <div id="filter-wrapper">
           <span className="date-filter-label"><b>Filter</b></span>
