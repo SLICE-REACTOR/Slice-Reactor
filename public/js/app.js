8f21cf3235276c7ca64a3b63e06adceb18b026e3
Robert Niimi <Robert Niimi>
2015-01-03 14:57:19 -0800

    deleted example data lines from app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,8 @@
 var App = require('./components/App.react');
 var React = require('react');
-var ExampleGraphData = require('./ExampleData');
 var APIUtils = require('./utils/APIUtils');
 window.React = React; // export for http://fb.me/react-devtools
 
-// loads example graph data
-ExampleGraphData.init();
 APIUtils.getAllOrders();
 
 React.render(
