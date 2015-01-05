d828b0667c93a989366f7354194d8e21172d0461
Krystal Raphael <krystal.raphael@gmail.com>
2015-01-05 14:06:45 -0800

    test node env

--- public/js/constants/Constants.js
+++ public/js/constants/Constants.js
@@ -1,15 +1,8 @@
 var keyMirror = require('keymirror');
 
-module.exports = {
+// module.exports = {
 
   ActionTypes: keyMirror({
     RECEIVE_GRAPH_DATA: null,
     FILTER_DATA: null
   }),
-
-  PayloadSources: keyMirror({
-    SERVER_ACTION: null,
-    VIEW_ACTION: null
-  })
-
-};
