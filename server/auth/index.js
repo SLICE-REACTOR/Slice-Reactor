3e1ec150f48fada9d679659738de98e600c572ed
Krystal Raphael <krystal.raphael@gmail.com>
2015-01-05 15:01:36 -0800

    test auth passed

--- server/auth/index.js
+++ server/auth/index.js
@@ -34,7 +34,6 @@ passport.use(new SliceStrategy({
         // store userid in the session
         req.session.UserId = user[0].dataValues.id;
         return done(null, user[0].dataValues.id);
-
      });
   }
 ));
