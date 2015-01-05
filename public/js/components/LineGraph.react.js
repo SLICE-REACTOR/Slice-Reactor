30c368ba841f4394c57ba1870bdcce4d8dafe2f5
Krystal Raphael <krystal.raphael@gmail.com>
2015-01-05 14:33:59 -0800

    template deployed

--- public/js/components/LineGraph.react.js
+++ public/js/components/LineGraph.react.js
@@ -1,8 +1,6 @@
 var React = require('react');
 var GraphDataStore = require('../stores/GraphDataStore');
 
-console.log('line.react');
-
 var getStateFromStores = function() {
   var sliceData = GraphDataStore.getData();
   var sliceDataMonthly = {};
