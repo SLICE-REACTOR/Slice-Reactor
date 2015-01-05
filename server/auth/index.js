8a06f67bfeac1c454ce152b334e76fd026238f66
Krystal Raphael <krystal.raphael@gmail.com>
2015-01-05 15:34:39 -0800

    uncomments line in db

--- server/auth/index.js
+++ server/auth/index.js
@@ -21,20 +21,20 @@ passport.use(new SliceStrategy({
   function(req, accessToken, refreshToken, profile, done) {
     var cipher = crypto.createCipher(process.env.CIPHER_ALGORITHM, process.env.CIPHER_KEY);
     req.session.accessToken = cipher.update(accessToken, 'utf8', 'hex') + cipher.final('hex');
-    // db.Users.findOrCreate({where: {userEmail: profile.userEmail}})
-    //  .then(function (user) {
-    //    user[0].dataValues.createTime = profile._json.result.createTime;
-    //    user[0].dataValues.firstName = profile.firstName;
-    //    user[0].dataValues.lastName = profile.lastName;
-    //    user[0].dataValues.updateTime = profile._json.result.updateTime;
-    //    user[0].dataValues.userName = profile._json.result.userName;
-    //    user[0].save();
 
-    //    // store userid in the session
-    //   // req.session.UserId = user[0].dataValues.id;
-    //    return done(null, user[0].dataValues.id);
-    //  });
-      return done(null, profile.userEmail);
+    db.Users.findOrCreate({where: {userEmail: profile.userEmail}})
+     .then(function (user) {
+       user[0].dataValues.createTime = profile._json.result.createTime;
+       user[0].dataValues.firstName = profile.firstName;
+       user[0].dataValues.lastName = profile.lastName;
+       user[0].dataValues.updateTime = profile._json.result.updateTime;
+       user[0].dataValues.userName = profile._json.result.userName;
+       user[0].save();
+
+       // store userid in the session
+      req.session.UserId = user[0].dataValues.id;
+      return done(null, user[0].dataValues.id);
+     });
   }
 ));
 
