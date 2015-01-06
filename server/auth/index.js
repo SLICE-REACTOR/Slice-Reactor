961a94a8ba0475708a4946322da92f720fdb2be3
Krystal Raphael <krystal.raphael@gmail.com>
2015-01-05 16:13:54 -0800

    restart heroku server

--- server/auth/index.js
+++ server/auth/index.js
@@ -34,6 +34,7 @@ passport.use(new SliceStrategy({
        // store userid in the session
       req.session.UserId = user[0].dataValues.id;
       return done(null, user[0].dataValues.id);
+      
      });
 
   }
