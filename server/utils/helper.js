f6fa44005129b20bc116d1ed3f2fefb50d9152f7
Krystal Raphael <krystal.raphael@gmail.com>
2015-01-05 17:35:47 -0800

    adds settimmout

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -125,8 +125,8 @@ var getUserData = function(req, res, next) {
   
   // last argument {limit: 1}
   sliceGetRequest('merchants', decryptedAccessToken, merchantsHandler, req.session.UserId);
-  sliceGetRequest('orders', decryptedAccessToken, ordersHandler, req.session.UserId);
-  sliceGetRequest('items', decryptedAccessToken, itemsHandler, req.session.UserId);
+  setTimeout(sliceGetRequest('orders', decryptedAccessToken, ordersHandler, req.session.UserId), 5000);
+  setTimeout(sliceGetRequest('items', decryptedAccessToken, itemsHandler, req.session.UserId), 15000);
 
   return next();
 };
