00f35588fda2aeae02b7a4bc0821fd51a64e576f
Krystal Raphael <krystal.raphael@gmail.com>
2015-01-06 11:40:35 -0800

    cleans files for intial master deploy

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -101,12 +101,8 @@ var getUserData = function(req, res, next) {
   
   // last argument {limit: 1}
   sliceGetRequest('merchants', decryptedAccessToken, merchantsHandler, req.session.UserId);
-  setTimeout(function(){
-    sliceGetRequest('orders', decryptedAccessToken, ordersHandler, req.session.UserId)
-  }, 5000);
-  setTimeout(function(){
-    sliceGetRequest('items', decryptedAccessToken, itemsHandler, req.session.UserId)
-  }, 15000);
+  sliceGetRequest('orders', decryptedAccessToken, ordersHandler, req.session.UserId);
+  sliceGetRequest('items', decryptedAccessToken, itemsHandler, req.session.UserId);
 
   return next();
 };
