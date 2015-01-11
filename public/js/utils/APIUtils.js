14a47c3bbc179b31b6ab4c578e07bf7b2d19295e
Rustin Crandall <rustinpc@gmail.com>
2015-01-11 11:10:10 -0800

    refactored login to do check for new user before data call; handle edge case of new user having just signed up for slice during login process and so has no data.

--- public/js/utils/APIUtils.js
+++ public/js/utils/APIUtils.js
@@ -3,8 +3,8 @@ var ServerActionCreators = require('../actions/ServerActionCreators');
 module.exports = {
   getAllGraphData: function() {
     $.get('/userdata', function(data, textStatus, xhr) {
-      if (xhr.status === 200 && data === 'ok') {
-        window.location = './loading';
+      if (xhr.status === 200 && data === 'empty') {
+        window.location = './newuser';
       } else {
         ServerActionCreators.receiveAllChartData(JSON.parse(data));
       }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,10 +3,15 @@ var helper = require('../utils/helper');
 module.exports = function(app, helper, db) {
 
   app.get('/', helper.ensureAuthenticated, function(req, res){
+    if (req.session.newUser) {
+      // must send response to data request from front end
+      res.redirect('./loading');
+    } else {
     fs.readFile(__dirname + '/../../public/home.html', 'utf8', function(err,data) {
       if (err) { throw err }
       res.send(data);
     });
+    }
   });
 
   app.get('/login', function(req, res){
@@ -23,25 +28,25 @@ module.exports = function(app, helper, db) {
 
   //handles get request from client
   app.get('/userdata', function(req, res){
-    if (req.session.newUser) {
-      // must send response to data request from front end
-      res.status(200).send('ok');
-    } else {
-      var user = req.session.UserId;
-      // query database for user info
-      db.Items.findAll({
-      attributes: ['purchaseDate', 'categoryName', 'price', 'quantity'],
-      where: {UserId: user},
-      include: [
-        {model: db.Orders, include: [
-          {model: db.Merchants, attributes: ['name']}
-        ], attributes: ['orderTotal']}
-      ]
-      }).then(function(items) {
-      // return user data to client
-        res.send(JSON.stringify(items))
-      });
-    }
+    var user = req.session.UserId;
+    // query database for user info
+    db.Items.findAll({
+    attributes: ['purchaseDate', 'categoryName', 'price', 'quantity'],
+    where: {UserId: user},
+    include: [
+      {model: db.Orders, include: [
+        {model: db.Merchants, attributes: ['name']}
+      ], attributes: ['orderTotal']}
+    ]
+    }).then(function(items) {
+      // if user has data return data to client
+      if (items.length > 0) {
+        res.send(JSON.stringify(items));
+      } else {
+        // must send response to data request from front end
+        res.status(200).send('empty');
+      }
+    });
   });
 
   app.get('/loaduserdata', helper.ensureAuthenticated, function(req, res) {
@@ -50,6 +55,13 @@ module.exports = function(app, helper, db) {
 
   app.get('/loading', helper.ensureAuthenticated, function(req, res) {
     fs.readFile(__dirname + '/../../public/loading.html', 'utf8', function(err,data) {
+      if (err) { throw err }
+      res.send(data);
+    });
+  });
+
+  app.get('/newuser', helper.ensureAuthenticated, function(req, res) {
+    fs.readFile(__dirname + '/../../public/newuser.html', 'utf8', function(err,data) {
       if (err) { throw err }
       res.send(data);
     });
