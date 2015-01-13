1629f4757dd9f1c4a98a6bffc18879b204aa9d08
Rustin Crandall <rustinpc@gmail.com>
2015-01-12 16:23:45 -0800

    move refresh token function back into helpers file; only calling function from new file run by Scheduler.

--- server/utils/helper.js
+++ server/utils/helper.js
@@ -234,7 +234,20 @@ var getUserData = function(userId, req, res) {
     });
 };
 
+// decrypt refresh token, renew access token, and update data in database
+var refreshUserAccessToken = function() {
+  db.Users.findAll()
+    .then(function (users) {
+      var apiInterval = 0;
+      users.forEach(function(user, key, collection) {
+        var decipher = crypto.createDecipher(process.env.CIPHER_ALGORITHM, process.env.CIPHER_KEY);
+        var decryptedRefreshToken = decipher.update(user.refreshToken, 'hex', 'utf8') + decipher.final('utf8');
+        setTimeout(function() {sliceRefreshRequest(decryptedRefreshToken, saveUpdatedTokens, user.id);}, apiInterval);
+        apiInterval += 15000; // space the API request for each user out by 15 seconds
+      });
+    });
+};
+
 module.exports.getUserData = getUserData;
-module.exports.saveUpdatedTokens = saveUpdatedTokens;
-module.exports.sliceRefreshRequest = sliceRefreshRequest;
+module.exports.refreshUserAccessToken = refreshUserAccessToken;
 module.exports.ensureAuthenticated = ensureAuthenticated;
--- server/utils/updateUserData.js
+++ server/utils/updateUserData.js
@@ -1,19 +1,3 @@
-var db = require('../db/sequelize');
-var crypto = require('crypto');
 var helper = require('./helper');
 
-// decrypt refresh token, renew access token, and update data in database
-var refreshUserAccessToken = function() {
-  db.Users.findAll()
-    .then(function (users) {
-      var apiInterval = 0;
-      users.forEach(function(user, key, collection) {
-        var decipher = crypto.createDecipher(process.env.CIPHER_ALGORITHM, process.env.CIPHER_KEY);
-        var decryptedRefreshToken = decipher.update(user.refreshToken, 'hex', 'utf8') + decipher.final('utf8');
-        setTimeout(function() {helper.sliceRefreshRequest(decryptedRefreshToken, helper.saveUpdatedTokens, user.id);}, apiInterval);
-        apiInterval += 15000; // space the API request for each user out by 15 seconds
-      });
-    });
-};
-
-refreshUserAccessToken();
+helper.refreshUserAccessToken();
