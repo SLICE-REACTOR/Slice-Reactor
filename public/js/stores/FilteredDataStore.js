b851fed31ab06b4d269446cacac365ebbede9f7a
Robert Niimi <Robert Niimi>
2015-01-14 11:59:12 -0800

    removed dateFilterHelper file

--- public/js/stores/FilteredDataStore.js
+++ public/js/stores/FilteredDataStore.js
@@ -2,7 +2,6 @@ var AppDispatcher = require('../dispatcher/Dispatcher');
 var Constants = require('../constants/Constants');
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
-var dateFilterHelpers = require('../utils/dateFilterHelpers');
 
 var ActionTypes = Constants.ActionTypes;
 var CHANGE_EVENT = 'change';
